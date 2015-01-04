9b2a3c4c00a9dc625da6e9ad14ac105c336a7786
Annabelle Richard <annabelle.e.richard@gmail.com>
2015-01-04 12:50:05 -0800

    Added things#show, completed thing_instance creation/editing.  Styled entity displays.

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -3,6 +3,13 @@ class ThingsController < ApplicationController
     @things = Thing.includes(instances: [ :space_time ]).all
   end
 
+  def show
+    @thing = Thing.find params[:id]
+    if @thing.nil?
+      head :not_found
+    end
+  end
+
   def new
     init_form_view_model(Thing.new)
   end
@@ -12,7 +19,7 @@ class ThingsController < ApplicationController
     thing.instances.each { |x| x.thing = thing }
     if thing.valid?
       thing.save
-      redirect_to thing, :action => :edit
+      redirect_to thing
     else
       logger.info 'Validation failed'
       logger.info thing.errors.to_json
@@ -25,6 +32,45 @@ class ThingsController < ApplicationController
     init_form_view_model(Thing.find params[:id])
   end
 
+  def update
+    thing = Thing.find params[:id]
+    if thing.update_attributes thing_params
+      redirect_to thing
+    else
+      logger.info 'Validation failed'
+      logger.info thing.errors.to_json
+      init_form_view_model(thing)
+      render 'edit'
+    end
+  end
+
+  def new_instance
+    thing = Thing.find params[:id]
+    if thing.nil?
+      head :not_found
+    end
+    init_instance_form_view_model(ThingInstance.new thing: thing)
+  end
+
+  def create_instance
+    thing = Thing.find params[:id]
+    if thing.nil?
+      head :not_found
+    else
+      instance = ThingInstance.new instance_params
+      instance.thing = thing
+      if instance.valid?
+        instance.save
+        redirect_to thing
+      else
+        logger.info 'Validation failed'
+        logger.info instance.errors.to_json
+        init_instance_form_view_model(instance)
+        render 'new_instance'
+      end
+    end
+  end
+
   private
   def init_form_view_model(thing)
     @space_times = SpaceTime.all
@@ -34,7 +80,16 @@ class ThingsController < ApplicationController
     end
   end
 
+  def init_instance_form_view_model(instance)
+    @space_times = SpaceTime.all
+    @instance = instance
+  end
+
   def thing_params
     params.require(:thing).permit(:name, :category, instances_attributes: [ :id, :space_time_id ])
+  end
+
+  def instance_params
+    params.require(:thing_instance).permit(:space_time_id)
   end
 end
